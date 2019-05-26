691182739b8ce41595e7405c8cd608d0820c8267
Justin <iceyang1007@gmail.com>
2019-05-26 08:10:55 +0800

    feat #1, a hash solution

--- algorithms/1-two-sum/main.go
+++ algorithms/1-two-sum/main.go
@@ -1,13 +1,23 @@
 package main
 
 func TwoSum(nums []int, target int) []int {
+	res := map[int]int{}
+	res[nums[0]] = 0
+	for i := 1; i < len(nums); i++ {
+		remain := target - nums[i]
+		remainIndex, exists := res[remain]
+		if exists {
+			return []int{remainIndex, i}
+		}
+		res[nums[i]] = i
+	}
 	return []int{0, len(nums) - 1}
 }
 
 /**
  * violent version.
  */
-func TwoSum(nums []int, target int) []int {
+func TwoSum1(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] == target {
--- algorithms/1-two-sum/main_test.go
+++ algorithms/1-two-sum/main_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestTwoSum(t *testing.T) {
-	nums := []int{-10, -1, -18, -19}
-	target := -19
+	nums := []int{2, 7, 11, 15}
+	target := 9
 	index := TwoSum(nums, target)
 	if nums[index[0]]+nums[index[1]] != target {
 		t.Errorf("expected %d but got result %d with index %+v. (input %+v)", target, nums[index[0]]+nums[index[1]], index, nums)
