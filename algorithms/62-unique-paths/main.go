a71a6196a897202d0651029ecd5737e31ca1abaf
Justin <iceyang1007@gmail.com>
2019-07-11 11:17:25 +0800

    feat #62. 不同路径, 增加dp2(降维)

--- algorithms/62-unique-paths/main.go
+++ algorithms/62-unique-paths/main.go
@@ -1,33 +1,45 @@
 package main
 
-import "fmt"
-
 func dp(m int, n int) int {
 	r := make([][]int, n)
 	for i, _ := range r {
 		r[i] = make([]int, m)
 		r[i][0] = 1
 	}
-
 	for i := 0; i < m; i++ {
 		r[0][i] = 1
 	}
-
 	for i := 1; i < n; i++ {
 		for j := 1; j < m; j++ {
 			r[i][j] = r[i][j-1] + r[i-1][j]
 		}
 	}
-
-	for _, v := range r {
-		fmt.Println(v)
-	}
 	return r[n-1][m-1]
+}
+
+func dp2(m int, n int) int {
+	r := make([]int, m)
+	for i, _ := range r {
+		r[i] = 1
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			r[j] += r[j-1]
+		}
+	}
+	return r[m-1]
+}
+
+func recursion(m, n int) int {
+	if m == 1 || n == 1 {
+		return 1
+	}
+	return recursion(m, n-1) + recursion(m-1, n)
 }
 
 func uniquePaths(m int, n int) int {
 	if m == 0 || n == 0 {
 		return 0
 	}
-	return dp(m, n)
+	return dp2(m, n)
 }
--- algorithms/62-unique-paths/main_test.go
+++ algorithms/62-unique-paths/main_test.go
@@ -3,6 +3,9 @@ package main
 import "testing"
 
 func TestUniquePaths(t *testing.T) {
+	if uniquePaths(7, 3) != 28 {
+		t.Fail()
+	}
 	if uniquePaths(0, 0) != 0 {
 		t.Fail()
 	}
@@ -12,7 +15,10 @@ func TestUniquePaths(t *testing.T) {
 	if uniquePaths(3, 2) != 3 {
 		t.Fail()
 	}
-	if uniquePaths(7, 3) != 28 {
+	if uniquePaths(20, 20) != 35345263800 {
+		t.Fail()
+	}
+	if uniquePaths(30, 23) != 156077261327400 {
 		t.Fail()
 	}
 }
