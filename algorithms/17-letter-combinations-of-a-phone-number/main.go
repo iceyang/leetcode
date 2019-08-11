953be26d2807ba92b70019503f19a9f1f03b3f5a
Justin <iceyang1007@gmail.com>
2019-08-12 00:11:16 +0800

    feat #17. letter-combinations-of-a-phone-number

--- algorithms/17-letter-combinations-of-a-phone-number/main.go
+++ algorithms/17-letter-combinations-of-a-phone-number/main.go
@@ -1,5 +1,34 @@
 package main
 
+var letters = map[byte]string{
+	'2': "abc",
+	'3': "def",
+	'4': "ghi",
+	'5': "jkl",
+	'6': "mno",
+	'7': "pqrs",
+	'8': "tuv",
+	'9': "wxyz",
+}
+
+var result []string
+
+func bt(str string, digits string) {
+	if len(digits) == 0 {
+		result = append(result, str)
+		return
+	}
+	number := digits[0]
+	for _, letter := range letters[number] {
+		bt(str+string(letter), digits[1:])
+	}
+}
+
 func letterCombinations(digits string) []string {
-	return []string{}
+	result = []string{}
+	if len(digits) == 0 {
+		return result
+	}
+	bt("", digits)
+	return result
 }
--- algorithms/17-letter-combinations-of-a-phone-number/main_test.go
+++ algorithms/17-letter-combinations-of-a-phone-number/main_test.go
@@ -3,4 +3,5 @@ package main
 import "testing"
 
 func TestLetterCombinations(t *testing.T) {
+	letterCombinations("23")
 }
