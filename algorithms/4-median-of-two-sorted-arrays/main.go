87df0dfd211bc976b7088b4b61dd7b43eaf275c2
Justin <iceyang1007@gmail.com>
2019-12-24 15:35:49 +0800

    feat #4.

--- algorithms/4-median-of-two-sorted-arrays/main.go
+++ algorithms/4-median-of-two-sorted-arrays/main.go
@@ -1,5 +1,45 @@
 package main
 
+import "math"
+
+func findKElementWithTwoSortedArrays(nums1 *[]int, nums2 *[]int, i int, j int, k int) int {
+	if i >= len((*nums1)) {
+		return (*nums2)[j+k-1]
+	}
+	if j >= len((*nums2)) {
+		return (*nums1)[i+k-1]
+	}
+	if k == 1 {
+		if (*nums1)[i] <= (*nums2)[j] {
+			return (*nums1)[i]
+		} else {
+			return (*nums2)[j]
+		}
+	}
+	num1 := math.MaxInt64
+	num2 := math.MaxInt64
+	if i+k/2-1 < len((*nums1)) {
+		num1 = (*nums1)[i+k/2-1]
+	}
+	if j+k/2-1 < len((*nums2)) {
+		num2 = (*nums2)[j+k/2-1]
+	}
+	if num1 < num2 {
+		return findKElementWithTwoSortedArrays(nums1, nums2, i+k/2, j, k-k/2)
+	} else {
+		return findKElementWithTwoSortedArrays(nums1, nums2, i, j+k/2, k-k/2)
+	}
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	length := len(nums1) + len(nums2)
+	result := findKElementWithTwoSortedArrays(&nums1, &nums2, 0, 0, (length+1)/2)
+	if length%2 == 0 {
+		return float64(result+findKElementWithTwoSortedArrays(&nums1, &nums2, 0, 0, (length+2)/2)) / 2
+	}
+	return float64(result)
+}
+
 // 时间复杂度未达到要求的版本
 func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
 	length1 := len(nums1)
